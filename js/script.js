3d6c0a358d8802220bc66b9186aae45e4086fba4
William Sosropartono <wcsosropartono@gmail.com>
2022-04-27 15:27:53 -0700

    Added buttons

---
+++ js/script.js
@@ -0,0 +1,47 @@
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a - b; 
+}
+
+function multiply(a,b){
+    return a*b;
+
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+
+function operate(operator, num1, num2){
+    switch(operator){
+        case "+": return add(num1,num2);
+        case "-": return subtract(num1,num2);
+        case "/": return divide(num1,num2);
+        case "*": return subtract(num1,num2);
+
+    }
+
+    // if(operator == "+"){
+    //     return add(num1,num2);
+    // }
+    // if(operator == "-"){
+    //     return subtract(num1,num2);
+    // }
+    // if(operator = "/"){
+    //     return divide(num1,num2);
+    // }
+    // if(operator = "*"){
+    //     return multiply(num1,num2);
+    // }
+
+}
+
+const buttonSelector = document.querySelector('button');
+buttonSelector.addEventListener('click', (event) => {
+    const displayValue = document.createElement('p');
+    console.log('button clicked!');
+})
