262ece6864a79dfcd2192980c0b154f514af8547
William Sosropartono <wcsosropartono@gmail.com>
2022-04-29 17:45:32 -0700

    Reworked eventListener functions
    Have a variable set to save the result of each operation

--- js/script.js
+++ js/script.js
@@ -40,6 +40,7 @@ function operate(operator, num1, num2){
 
 }
 
+//Clear function
 function clear(){
     displaySelector.innerHTML = '';
 }
@@ -49,29 +50,33 @@ const numSelector = document.querySelectorAll('.num');
 const opSelector = document.querySelectorAll('.op');
 const clrSelector = document.querySelector('.clr');
 const displaySelector = document.querySelector('.display');
+const equalSelector = document.querySelector('.equal');
 
-//Allows for the display of any number. A number button must be clicked before operand/clear/equate buttons are usable
-numSelector.forEach(event => event.addEventListener('click', event => {
-    //Adds event target's inner HTML to the display
-       displaySelector.innerHTML += event.target.innerHTML;
-       let firstNum;
-       //collects operand
-       let sym = opSelector.forEach(event => event.addEventListener('click', event => {
-        firstNum = displaySelector.innerHTML;
-           clear();
-           return event.target.innerHTML;
-       }))
+// Algorithm: receive first input from the number/Keyboard
+// Get operator input
+// Once operator input is selected, remove first eventListener and save second input from number/Keyboard
+// equate for the answer, and return a string to display
 
-
-
-    //    console.log(operate(sym,firstNum,secondNum));
-
-       //collect/listen to the second number and store it
-
-
-
-
-
-
+//Receive first input
+// forEach method is applied to add eventListeners to nodelist
+const firstNumber = numSelector.forEach(number => number.addEventListener('click', event => {
+    //display the first number
+    displaySelector.innerHTML +=  event.target.innerHTML;
+    return displaySelector.innerHTML
 }))
+
+
+
+const operator = opSelector.forEach(operator => operator.addEventListener('click', event => {
+    return event.target.innerHTML;
+}));
+
+equalSelector.addEventListener('click', event => {
+    displaySelector.innerHTML = operate(firstNumber,secondNumber, operator);
+})
+
+//Clear button changes display innerHTML to empty string
+clrSelector.addEventListener('click', event => {
+    displaySelector.innerHTML = '';
+});
 
