432e70221db89982258dfdbd14661a186ae09033
William Sosropartono <wcsosropartono@gmail.com>
2022-05-14 20:51:07 -0700

    Implemented arrays to save number inputs
    Clicking or inputting a number after choosing an operator will
    keep the current displayed number until another number is inputted

--- js/script.js
+++ js/script.js
@@ -1,29 +1,30 @@
-function add(a,b){
-    return a+b;
+function add(a, b) {
+    return (+a + +b);
 }
 
-function subtract(a,b){
-    return a - b; 
+function subtract(a, b) {
+    return +a - +b;
 }
 
-function multiply(a,b){
-    return a*b;
-
+function multiply(a, b) {
+    return +a*+b;
 }
 
-function divide(a,b){
-    return a/b;
+function divide(a, b) {
+    return +a / +b;
 }
 
 
-function operate(operator, num1, num2){
-    switch(operator){
-        case "+": return add(num1,num2);
-        case "-": return subtract(num1,num2);
-        case "/": return divide(num1,num2);
-        case "*": return subtract(num1,num2);
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case "+": return add(num1, num2);
+        case "-": return subtract(num1, num2);
+        case "/": return divide(num1, num2);
+        case "*": return multiply(num1, num2);
 
     }
+
+    //if statement operation selection
 
     // if(operator == "+"){
     //     return add(num1,num2);
@@ -41,7 +42,7 @@ function operate(operator, num1, num2){
 }
 
 //Clear function
-function clear(){
+function clear() {
     displaySelector.innerHTML = '';
 }
 
@@ -51,32 +52,82 @@ const opSelector = document.querySelectorAll('.op');
 const clrSelector = document.querySelector('.clr');
 const displaySelector = document.querySelector('.display');
 const equalSelector = document.querySelector('.equal');
+const displayOP = document.querySelector('.displayOP');
 
-// Algorithm: receive first input from the number/Keyboard
+// Algorithm: Receive first input from the clickable buttons/Keyboard
 // Get operator input
-// Once operator input is selected, remove first eventListener and save second input from number/Keyboard
-// equate for the answer, and return a string to display
+// Save first number
+// Get and save second number
+// Operate when operate button is clicked
+// Return a string to display
+
+let numberArray = [];
+let operatorArray = [];
+
 
 //Receive first input
 // forEach method is applied to add eventListeners to nodelist
-const firstNumber = numSelector.forEach(number => number.addEventListener('click', event => {
-    //display the first number
-    displaySelector.innerHTML +=  event.target.innerHTML;
-    return displaySelector.innerHTML
+
+document.addEventListener('keydown', keyown);
+
+numSelector.forEach(number => number.addEventListener('click', clicking));
+
+
+function keyown(event){
+    if (operatorArray.length && displaySelector.innerHTML == numberArray[numberArray.length - 1]) {
+        clearDisplay();
+        numberArray.push(0);
+    }
+    displaySelector.innerHTML += event.key;
+
+}
+
+// Click Function
+//After clicking on an Operator, the number display will stay the same until clicking another number, it checks to see if the display is the same as the number pushed, 
+//that way it clears it once another number is inputted rather than everytime it is clicked
+function clicking(event) {
+    if (operatorArray.length && displaySelector.innerHTML == numberArray[numberArray.length - 1]) {
+        clearDisplay();
+        numberArray.push(0);
+    }
+    displaySelector.innerHTML += event.target.innerHTML;
+}
+
+
+//Operator listener, pushes first number into array
+opSelector.forEach(operator => operator.addEventListener('click', event => {
+    numberArray.push(displaySelector.innerHTML);
+    operatorArray.push(event.target.innerHTML);
+}));
+
+//Keyboard listener
+opSelector.forEach(operator => operator.addEventListener('keypress', event => {
+    numberArray.push(displaySelector.innerHTML);
+    operatorArray.push(event.target.innerHTML);
 }))
 
 
-
-const operator = opSelector.forEach(operator => operator.addEventListener('click', event => {
-    return event.target.innerHTML;
-}));
-
+//firstNumber assigns latest firstNumber, secondNumber assigns number after 0
 equalSelector.addEventListener('click', event => {
-    displaySelector.innerHTML = operate(firstNumber,secondNumber, operator);
+    numberArray.push(displaySelector.innerHTML);
+    operator = operatorArray[operatorArray.length - 1];
+    firstNumber = numberArray[numberArray.length - 3];
+    secondNumber = numberArray[numberArray.length - 1];
+    displaySelector.innerHTML = operate(operator, firstNumber, secondNumber);
 })
 
 //Clear button changes display innerHTML to empty string
-clrSelector.addEventListener('click', event => {
+clrSelector.addEventListener('click', clearAll);
+
+//Clear both display and arrays function
+function clearAll() {
     displaySelector.innerHTML = '';
-});
+    numberArray = [];
+    operatorArray = [];
+}
+
+//Clear display function
+function clearDisplay() {
+    displaySelector.innerHTML = '';
+}
 
